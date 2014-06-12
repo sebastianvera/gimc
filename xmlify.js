e553ca8d895ff72c7e77506773047c8d6ab409c1
Sebastian Vera <u.verainf@gmail.com>
2014-06-12 01:09:56 -0400

    Add xmlify and comment jsonify for precaution

---
+++ xmlify.js
@@ -0,0 +1,34 @@
+var jx = require('js2xmlparser'),
+    fs = require('fs-extra'),
+    jf = require('jsonfile'),
+    _ = require('lodash');
+
+var path = './classes';
+var jsonPath = function(file) {
+  return path+"/"+file
+}
+var xmlPath = function (json) {
+  var xmlFile = json.replace('.json', '.xml')
+  return path+"/"+xmlFile;
+}
+
+var jsons = fs.readdirSync(path);
+jsons = _.filter(jsons, function(json){
+  return json.match(/\.json/);
+});
+
+// use just first 20
+jsons = _.first(jsons, 20);
+
+// set xml options
+var xml_options = {
+  declaration: {
+    include: false
+  }
+}
+
+_.each(jsons, function (fileName) {
+  var json = jf.readFileSync(jsonPath(fileName));
+  var xml = jx("annotation", json, xml_options);
+  fs.writeFileSync(xmlPath(fileName), xml);
+});
